886776564666bcc80ccd68d497c5a72bfd5ffdc1
Philippe Quesnel <philippe.quesnel@gmail.com>
2013-07-26 15:49:26 -0400

    initial commit

---
+++ dirWalker1.go/src/code.google.com/p/dirWalker1/dirWalker/dirWalker1.go
@@ -0,0 +1,81 @@
+// dirWalker1.go
+package main
+
+import (
+	"code.google.com/p/dirWalker1/dirtree"
+	"fmt"
+	"html"
+	"strings"
+)
+
+//----------
+
+// DirTreePrinter: output to stdout, indented
+
+type DirPrint struct {
+	depth int
+}
+
+func (d *DirPrint) spaces() string {
+	nbSpaces := d.depth * 3
+	return strings.Repeat(" ", nbSpaces)
+}
+
+func (d *DirPrint) EnterDir(name string) {
+	fmt.Printf("%s+%s/\n", d.spaces(), name)
+	d.depth++
+}
+
+func (d *DirPrint) ExitDir(name string) {
+	d.depth--
+}
+
+func (d *DirPrint) DoFile(name string) {
+	fmt.Printf("%s%s\n", d.spaces(), name)
+}
+
+//----------
+
+// DirTreePrinter: output to stdout, as XML
+
+type DirPrintXml struct {
+	depth int
+}
+
+func (d *DirPrintXml) spaces() string {
+	nbSpaces := d.depth * 3
+	return strings.Repeat(" ", nbSpaces)
+}
+
+func (d *DirPrintXml) EnterDir(name string) {
+	fmt.Printf("%s<dir name=\"%s\">\n", d.spaces(), html.EscapeString(name))
+	d.depth++
+}
+
+func (d *DirPrintXml) ExitDir(name string) {
+	d.depth--
+	fmt.Printf("%s</dir>\n", d.spaces())
+}
+
+func (d *DirPrintXml) DoFile(name string) {
+	fmt.Printf("%s<file>%s</file>\n", d.spaces(), html.EscapeString(name))
+}
+
+//----------
+
+func main() {
+	// read tree
+	if tree, err := dirtree.New("/home/kwez/Videos"); err == nil {
+		fmt.Println(tree)
+
+		// output indented
+		tree.Walk(&DirPrint{})
+
+		// output as XML
+		tree.Walk(&DirPrintXml{})
+
+	} else {
+		fmt.Println(err)
+	}
+
+}
---
+++ dirWalker1.go/src/code.google.com/p/dirWalker1/dirtree/dirtree.go
@@ -0,0 +1,81 @@
+// dirtree.go
+package dirtree
+
+import "io/ioutil"
+import "path"
+
+// definition of DirTree, holds the directory structure
+type DirTree struct {
+	name  string
+	kids  []*DirTree
+	files []string
+}
+
+// interface to implement to print the dirs/files of DirTree (DirTree.Walk())
+type DirTreePrinter interface {
+	EnterDir(name string)
+	ExitDir(name string)
+	DoFile(name string)
+}
+
+//----------
+
+// Create a DirTree from top
+func New(dirpath string) (*DirTree, error) {
+	tree := &DirTree{}
+
+	if err := tree.read(dirpath); err != nil {
+		return nil, err
+	}
+
+	return tree, nil
+}
+
+//----------
+
+// traverse DirTree, calling the DirTreePrinter on dirs / files
+func (tree *DirTree) Walk(printer DirTreePrinter) {
+	if tree != nil {
+		printer.EnterDir(tree.name)
+
+		for _, kid := range tree.kids {
+			kid.Walk(printer)
+		}
+
+		for _, file := range tree.files {
+			printer.DoFile(file)
+		}
+
+		printer.ExitDir(tree.name)
+	}
+}
+
+//----------
+
+// read recursively the directory, creating DirTrees
+func (tree *DirTree) read(dirpath string) error {
+	// get base name ie "subdir1"
+	tree.name = path.Base(dirpath)
+
+	// read directory
+	dirEntries, err := ioutil.ReadDir(dirpath)
+	if err != nil {
+		return err
+	}
+
+	// process entries, file or subdir
+	for _, v := range dirEntries {
+		if v.IsDir() {
+			// recurse, read subdir
+			kid, err := New(path.Join(dirpath, v.Name()))
+			if err != nil {
+				return err
+			}
+			tree.kids = append(tree.kids, kid)
+		} else {
+			tree.files = append(tree.files, v.Name())
+		}
+	}
+
+	return nil // no error
+}
